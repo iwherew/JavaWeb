b4541fcbed6c20f4cd075e1422f086423cd5e56c
DESKTOP-2HR778K\123 <iwhere_w@126.com>
2022-03-15 22:23:31 +0800

    JDBC

--- LearnJDBC/jdbc-demo/src/com/jdbc/JDBCDemo.java
+++ LearnJDBC/jdbc-demo/src/com/jdbc/JDBCDemo.java
@@ -11,9 +11,14 @@ import java.sql.Statement;
 public class JDBCDemo {
     public static void main(String[] args) throws Exception{
         // 1、注册驱动
-        Class.forName("com.mysql.jdbc.Driver");
+        // MySQL 5之后的驱动包，可以省略注册驱动的步骤
+        // 自动加载jar包中 META-INF/services/java.sql.Driver 文件中的驱动类
+//        Class.forName("com.mysql.jdbc.Driver");
 
         // 2、获取连接
+        // 如果连接的是本机MySQL并且端口号是默认的3306，可以简化书写
+//        String url = "jdbc:mysql:///db1?useSSL=false";
+        // useSSL=false 禁用警告提示
         String url = "jdbc:mysql://127.0.0.1:3306/db1?useSSL=false";
         String username = "root";
         String password = "123456";
