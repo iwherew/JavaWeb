ccab58c6ba118003d61634e28646233b7f6345ee
DESKTOP-2HR778K\123 <iwhere_w@126.com>
2022-03-15 21:24:26 +0800

    JDBC

---
+++ LearnJDBC/jdbc-demo/src/com/jdbc/JDBCDemo.java
@@ -0,0 +1,38 @@
+package com.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+/**
+ * JDBC 快速入门
+ * */
+
+public class JDBCDemo {
+    public static void main(String[] args) throws Exception{
+        // 1、注册驱动
+        Class.forName("com.mysql.jdbc.Driver");
+
+        // 2、获取连接
+        String url = "jdbc:mysql://127.0.0.1:3306/db1?useSSL=false";
+        String username = "root";
+        String password = "123456";
+        Connection conn = DriverManager.getConnection(url, username, password);
+
+        // 3、定义sql语句
+        String sql = "update account set money = 4000 where id = 1";
+
+        // 4、获取执行sql的对象 Statement
+        Statement stmt = conn.createStatement();
+
+        // 5、执行sql
+        int count = stmt.executeUpdate(sql);
+
+        // 6、处理结果
+        System.out.println(count);
+
+        // 7、释放资源
+        stmt.close();
+        conn.close();
+    }
+}
